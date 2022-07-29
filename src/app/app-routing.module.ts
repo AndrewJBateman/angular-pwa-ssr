4a2af7f3267e036a7f7159c24b8cbb5ae73c9f7d
AndrewJBateman <gomezbateman@yahoo.com>
2022-07-29 19:11:12 +0200

    fix: enabledBlocking replaces enabled to fix error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,7 +27,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      initialNavigation: 'enabled',
+      initialNavigation: 'enabledBlocking',
     }),
   ],
   exports: [RouterModule],
