e1b99eef531d5843c6b2cdea4837ec5a6dc76d36
AndrewJBateman <gomezbateman@yahoo.com>
2022-07-30 11:01:28 +0200

    chore: ng dependency & JS version updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,35 +1,35 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { HomeComponent } from './components/home/home.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HomeComponent } from "./components/home/home.component";
+import { NotFoundComponent } from "./components/not-found/not-found.component";
 
 const routes: Routes = [
   {
-    path: '',
-    component: HomeComponent,
+    path: "",
+    component: HomeComponent
   },
   {
-    path: 'about',
+    path: "about",
     loadChildren: () =>
-      import('./components/about/about.module').then((mod) => mod.AboutModule),
+      import("./components/about/about.module").then(mod => mod.AboutModule)
   },
   {
-    path: 'contact',
+    path: "contact",
     loadChildren: () =>
-      import('./components/contact/contact.module').then(
-        (mod) => mod.ContactModule
-      ),
+      import("./components/contact/contact.module").then(
+        mod => mod.ContactModule
+      )
   },
-  { path: '**', component: NotFoundComponent },
+  { path: "**", component: NotFoundComponent }
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      initialNavigation: 'enabledBlocking',
-    }),
+      initialNavigation: "enabledBlocking"
+    })
   ],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
