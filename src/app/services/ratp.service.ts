d9bc9720e7cc03524cf4ab97c755aa06020c5e2a
AndrewJBateman <gomezbateman@yahoo.com>
2022-10-05 13:14:38 +0200

    fix: RATP API dataset name updated

--- src/app/services/ratp.service.ts
+++ src/app/services/ratp.service.ts
@@ -1,30 +1,27 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { throwError, Observable } from 'rxjs';
-import { catchError, take } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { throwError, Observable } from "rxjs";
+import { catchError, take } from "rxjs/operators";
 
-import { RatpResponse } from '../models/ratp';
+import { RatpResponse } from "../models/ratp";
 
-const baseUrl = 'https://data.ratp.fr/api/records/1.0/search/?';
+const baseUrl = "https://data.ratp.fr/api/records/1.0/search/?";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root"
 })
 export class RatpService {
-  private ratpResponseData?: Observable<RatpResponse>
-  constructor(
-    private http: HttpClient,
-
-  ) {}
+  private ratpResponseData?: Observable<RatpResponse>;
+  constructor(private http: HttpClient) {}
 
   getRatpData(query: string): Observable<RatpResponse> {
-    const userSearchUrl = `${baseUrl}dataset=liste-des-commerces-de-proximite-agrees-ratp&q=${query}&rows=1052&sort=-code_postal&facet=tco_libelle&facet=code_postal`;
+    const userSearchUrl = `${baseUrl}dataset=commerces-de-proximite-agrees-ratp&q=${query}&rows=1052&sort=-code_postal&facet=tco_libelle&facet=code_postal`;
     this.ratpResponseData = this.http.get<RatpResponse>(userSearchUrl).pipe(
       take(1),
-      catchError((err) => {
+      catchError(err => {
         return throwError(() =>
           console.log(
-            'There was a problem fetching data from the RATP API, error: ',
+            "There was a problem fetching data from the RATP API, error: ",
             err
           )
         );
